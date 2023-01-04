f65b70ba04ed721ecfc255cdabbfa37d1cbee5ab
TinF9 <martin.h4628@gmail.com>
2023-01-04 00:31:40 -0300

    Upgraded to v2.0.0: -Upgraded the way data input is done: changed from a JSON format text input, to an excel file input. -Updated README file according to the upgrade. -Other minor fixes.

--- README.md
+++ README.md
@@ -1,46 +1,26 @@
 # Logistics 1
+
 ![Logistics1 image](/Logistics1-image.png)
+
 ## Description
-This is a logistics assistance project I developed for my brother. He is an engineer working for a logistics company in Spain.
+
+This is a logistics assistance project I developed for my brother. He is an engineer working for a logistics company.
 
 He needed aid dealing with a large number of messy products. His target is to place each product into its correct destination according to the data he is provided about each one of them (product code, quantity and destination).
 
 So what he needs is to take a messy product, to check for its code, and to have an easy and fast way to find it over the data so that he knows where to place it.
 
 Finally, he wanted the program to count the already handled products, and to tell when a product is finished.
-## How to use it
-1. The trickiest part is at the very beginning, when you have to input the data you are going to be working with. Once you surpass this step, it becomes all super intuitive. It is not the best way to do it for sure, but it was part of the challenge and fun to see it working.
-   1. You need to input text in the *dialog box*, but that text must be in a specific format for the program to work:
-      - Firstly, it must be in **JSON format**.
-      - Secondly, it must contain the appropriate data with the **appropriate keys** for each product ("Product Code", "Quantity", "Placed", "Not Placed", "Destination List").
-      
-      Here is an example containing data for two products:
-      ```
-        [
-         {
-          "Product Code": "ASD654",
-          "Quantity": 4,
-          "Placed": null,
-          "Not Placed": null,
-          "Destination List": "List 1"
-         },
-         {
-          "Product Code": "AS3D2G",
-          "Quantity": 6,
-          "Placed": null,
-          "Not Placed": null,
-          "Destination List": "List 5"
-         }
-        ]
-      ```
-      I have also included a `.txt` file called `"InitialDataJsonExample.txt"` in the repository, which contains data for many products in the appropriate JSON format (you can copy-paste it all exactly as it is into the dialog box); and an `.xlsx` file called `"InitialDataExcelExample.xlsx"` to show where the data come from in a table format.
 
-   2. Hit the Ok button.
-   
+Only one library is integrated to handle data input from an `.xlsx` file. The rest of the script is Vanilla JavaScript.
+
+## How to use it
+
+1. You have to input the data you are going to be working with. This is done by selecting a proper `.xlsx` file. The file must contain the appropriate data with the **appropriate keys** for each product ("Product Code", "Quantity", "Placed", "Not Placed", "Destination List"). I have included an `.xlsx` file called `"InitialDataExcelExample.xlsx"` in the repository as an example.
 2. The products and their values are arranged into a table for the user to have a comfortable view of them.
 3. From the search box, you can select a product to work with and to see its values.
 4. Use the buttons to count the products or to print the table.
 
-*You can see the program uploaded as a website **[here](https://tinf9.github.io/Logistics1/)**.*
+_You can see the program uploaded as a website **[here](https://tinf9.github.io/Logistics1/)**._
 
 **Thank you very much for checking it out.**
--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+const batchSelection = document.getElementById("batchSelection");
+const flexContainer = document.querySelector(".flex-container");
+const fileInput = document.getElementById("fileInput");
+const batchIndicator = document.getElementById("batchIndicator");
+const excelProducts = [];
+let products = "";
 
-const productsJsonString =
-prompt("Please input the products to work with and their values in JSON text format");
+fileInput.addEventListener("change", async function () {
+  const excelData = await readXlsxFile(fileInput.files[0]);
+  let fileName = fileInput.files[0].name;
+
+  for (let i = 1; i < excelData.length; i++) {
+    const product = {
+      "Product Code": excelData[i][0],
+      Quantity: excelData[i][1],
+      Placed: excelData[i][2],
+      "Not Placed": excelData[i][3],
+      "Destination List": excelData[i][4],
+    };
+    excelProducts.push(product);
+  }
+
+  products = [...excelProducts];
+  initialCount();
+  createOptionsList();
+  createTableContent();
+
+  batchIndicator.innerHTML = `<p style="color:#888;">Working with: '${fileName}' batch.</p>`;
+
+  batchSelection.style.display = "none";
+  flexContainer.style.display = "flex";
+});
 
 const form1 = document.getElementById("form1");
 const searcher = document.getElementById("searcher");
@@ -12,12 +41,12 @@ const button5 = document.getElementById("button5");
 const productDetails = document.getElementById("productDetails");
 
 const noProduct = {
-     "Product Code": "Unregistered product",
-     "Quantity": "------",
-     "Placed": "------",
-     "Not Placed": "------",
-     "Destination List": "------"
-    };
+  "Product Code": "Unregistered product",
+  Quantity: "------",
+  Placed: "------",
+  "Not Placed": "------",
+  "Destination List": "------",
+};
 
 const initialProduct = `
    <ul>
@@ -35,12 +64,12 @@ const disableButtons = () => {
   button2.disabled = true;
   button3.disabled = true;
   button4.disabled = true;
-  
+
   button1.classList.add("disabled");
   button2.classList.add("disabled");
   button3.classList.add("disabled");
   button4.classList.add("disabled");
-}
+};
 
 disableButtons();
 
@@ -49,189 +78,160 @@ const enableButtons = () => {
   button2.disabled = false;
   button3.disabled = false;
   button4.disabled = false;
-  
+
   button1.classList.remove("disabled");
   button2.classList.remove("disabled");
   button3.classList.remove("disabled");
   button4.classList.remove("disabled");
-}
+};
 
-const disableSubtractButton = selectedProduct => {
-   if(selectedProduct["Placed"] <= 0) {
-      button2.disabled = true;
-      button2.classList.add("disabled");
-   }
-}
+const disableSubtractButton = (selectedProduct) => {
+  if (selectedProduct["Placed"] <= 0) {
+    button2.disabled = true;
+    button2.classList.add("disabled");
+  }
+};
 
 const disableInput = () => {
-   searcher.disabled = true;
-   searcher.classList.add("disabled");
-}
-
-const checkJsonParse = () => {
-   try {
-      JSON.parse(productsJsonString);
-   }
-   catch {
-      disableInput();
-      disableButtons();
-      productDetails.innerHTML = `<b style="color:red">No valid JSON format entered.<br>
-      Please refresh the page to try again.</b>`;
-   }
-}
-
-const checkPrompt = () => {
-   if(productsJsonString === null || productsJsonString === "") {
-      disableInput();
-      disableButtons();
-      productDetails.innerHTML = `<b style="color:red">No products have been loaded.<br>
-      Please refresh the page to input the products.</b>`;
-   } else {checkJsonParse();}
-}
-
-checkPrompt();
-
-const products = (function() {
-   try {
-      if(JSON.parse(productsJsonString) === null) {
-         return [noProduct];
-      } else {return JSON.parse(productsJsonString);}
-   }
-   catch {return [noProduct];}
-})();
+  searcher.disabled = true;
+  searcher.classList.add("disabled");
+};
 
 const initialCount = () => {
-   products.forEach(function(product) {
-      if(product["Placed"] === null) {
-         product["Placed"] = 0;
-         product["Not Placed"] = product["Quantity"];
-      }
-   })
-}
-
-initialCount();
-
-const countToZeroAll = () => {
-   products.forEach(function(product) {
+  products.forEach(function (product) {
+    if (product["Placed"] === null) {
       product["Placed"] = 0;
       product["Not Placed"] = product["Quantity"];
-   })
-}
+    }
+  });
+};
 
-const checkSelectedP = searcherValue => {
-   let selectedProduct;
-   for (let i = 0; i < products.length; i++) {
-      if(products[i]["Product Code"] == searcherValue) {
-         selectedProduct = products[i];
-         enableButtons();
-         break;
-      } else {
-         selectedProduct = noProduct;
-         disableButtons();}
-   }
-   return selectedProduct;
-}
+const countToZeroAll = () => {
+  products.forEach(function (product) {
+    product["Placed"] = 0;
+    product["Not Placed"] = product["Quantity"];
+  });
+};
+
+const checkSelectedP = (searcherValue) => {
+  let selectedProduct;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i]["Product Code"] == searcherValue) {
+      selectedProduct = products[i];
+      enableButtons();
+      break;
+    } else {
+      selectedProduct = noProduct;
+      disableButtons();
+    }
+  }
+  return selectedProduct;
+};
 
 const alertCompleted = document.querySelector(".alert--fully-placed");
 const alertSurpassed = document.querySelector(".alert--overstock");
 
-const alertCount = selectedProduct => {
-   if(selectedProduct["Not Placed"] <= 0) {
-      alertCompleted.style.display = "inline-block";
-   } else {alertCompleted.style.display = "none";}
+const alertCount = (selectedProduct) => {
+  if (selectedProduct["Not Placed"] <= 0) {
+    alertCompleted.style.display = "inline-block";
+  } else {
+    alertCompleted.style.display = "none";
+  }
 
-   if(selectedProduct["Not Placed"] < 0) {
-      alertSurpassed.style.display = "inline-block";
-   } else {alertSurpassed.style.display = "none";}
+  if (selectedProduct["Not Placed"] < 0) {
+    alertSurpassed.style.display = "inline-block";
+  } else {
+    alertSurpassed.style.display = "none";
+  }
 
-   disableSubtractButton(selectedProduct);
-}
+  disableSubtractButton(selectedProduct);
+};
 
-const resetCount = searcherValue => {
-   let confirmation = confirm(`The counting of all products will be restarted.
+const resetCount = (searcherValue) => {
+  let confirmation = confirm(`The counting of all products will be restarted.
 Do you want to continue?`);
-   if(confirmation) {
-      countToZeroAll();
-      showProduct(searcher.value);
-      for (let i = 0; i < products.length; i++) {
-         let iD = "product" + i;
-         let selectedRow = document.getElementById(iD);
-         selectedRow.innerHTML = `
+  if (confirmation) {
+    countToZeroAll();
+    showProduct(searcher.value);
+    for (let i = 0; i < products.length; i++) {
+      let iD = "product" + i;
+      let selectedRow = document.getElementById(iD);
+      selectedRow.innerHTML = `
             <td>${products[i]["Product Code"]}</td>
             <td>${products[i]["Quantity"]}</td>
             <td>${products[i]["Placed"]}</td>
             <td>${products[i]["Not Placed"]}</td>
             <td>${products[i]["Destination List"]}</td>`;
-      }
-   }
-}
+    }
+  }
+};
 
-const resetProduct = searcherValue => {
-   let confirmation = confirm(`The counting of this product will be restarted.
+const resetProduct = (searcherValue) => {
+  let confirmation = confirm(`The counting of this product will be restarted.
 Do you want to continue?`);
-   if(confirmation) {
-      let selectedProduct = checkSelectedP(searcherValue);
-      selectedProduct["Placed"] = 0;
-      selectedProduct["Not Placed"] = selectedProduct["Quantity"];
-      showProduct(searcher.value);
-   }
-}
+  if (confirmation) {
+    let selectedProduct = checkSelectedP(searcherValue);
+    selectedProduct["Placed"] = 0;
+    selectedProduct["Not Placed"] = selectedProduct["Quantity"];
+    showProduct(searcher.value);
+  }
+};
 
-const addProduct = searcherValue => {
-   let selectedProduct = checkSelectedP(searcherValue);
-   selectedProduct["Placed"] += 1;
-   selectedProduct["Not Placed"] -= 1;
-   showProduct(searcher.value);
-}
+const addProduct = (searcherValue) => {
+  let selectedProduct = checkSelectedP(searcherValue);
+  selectedProduct["Placed"] += 1;
+  selectedProduct["Not Placed"] -= 1;
+  showProduct(searcher.value);
+};
 
-const subtractProduct = searcherValue => {
-   let selectedProduct = checkSelectedP(searcherValue);
-   selectedProduct["Placed"] -= 1;
-   selectedProduct["Not Placed"] += 1;
-   showProduct(searcher.value);
-}
+const subtractProduct = (searcherValue) => {
+  let selectedProduct = checkSelectedP(searcherValue);
+  selectedProduct["Placed"] -= 1;
+  selectedProduct["Not Placed"] += 1;
+  showProduct(searcher.value);
+};
 
-const checkSelectedPi = searcherValue => {
-   for (let i = 0; i < products.length; i++) {
-      if(products[i]["Product Code"] == searcherValue) {
-         return i;
-      }
-   }
-}
+const checkSelectedPi = (searcherValue) => {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i]["Product Code"] == searcherValue) {
+      return i;
+    }
+  }
+};
 
-const replaceRow = searcherValue => {
-   let i = checkSelectedPi(searcherValue);
-   let iD = "product" + i;
-   if(i != undefined) {
-      let selectedRow = document.getElementById(iD);
-      if(products[i]["Not Placed"] == 0) {
-         selectedRow.innerHTML = `
+const replaceRow = (searcherValue) => {
+  let i = checkSelectedPi(searcherValue);
+  let iD = "product" + i;
+  if (i != undefined) {
+    let selectedRow = document.getElementById(iD);
+    if (products[i]["Not Placed"] == 0) {
+      selectedRow.innerHTML = `
             <td>${products[i]["Product Code"]}</td>
             <td>${products[i]["Quantity"]}</td>
             <td>${products[i]["Placed"]}</td>
             <td style="color:#ffff00; background:#ff0000; font-weight:bolder; font-family:sans-serif">${products[i]["Not Placed"]}</td>
             <td>${products[i]["Destination List"]}</td>`;
-      } else if(products[i]["Not Placed"] < 0) {
-         selectedRow.innerHTML = `
+    } else if (products[i]["Not Placed"] < 0) {
+      selectedRow.innerHTML = `
             <td>${products[i]["Product Code"]}</td>
             <td>${products[i]["Quantity"]}</td>
             <td>${products[i]["Placed"]}</td>
             <td style="color:#f3f3f3; background:#0000ff; font-weight:bolder; font-family:sans-serif">${products[i]["Not Placed"]}</td>
             <td>${products[i]["Destination List"]}</td>`;
-      } else {
-         selectedRow.innerHTML = `
+    } else {
+      selectedRow.innerHTML = `
             <td>${products[i]["Product Code"]}</td>
             <td>${products[i]["Quantity"]}</td>
             <td>${products[i]["Placed"]}</td>
             <td>${products[i]["Not Placed"]}</td>
             <td>${products[i]["Destination List"]}</td>`;
-      }
-   }
-}
+    }
+  }
+};
 
-const showProduct = searcherValue => {
-   let selectedProduct = checkSelectedP(searcherValue);
-   productDetails.innerHTML = `
+const showProduct = (searcherValue) => {
+  let selectedProduct = checkSelectedP(searcherValue);
+  productDetails.innerHTML = `
    <ul>
       <li><b>Product:</b>   <b style="color:red">
       ${selectedProduct["Product Code"]}</b></li>
@@ -241,67 +241,83 @@ const showProduct = searcherValue => {
       <li><b>Destination List:</b>  <b style="color:blue;
       font-family:sans-serif">${selectedProduct["Destination List"]}</b></li>
    </ul>`;
-   alertCount(selectedProduct);
-   replaceRow(searcherValue);
-}
+  alertCount(selectedProduct);
+  replaceRow(searcherValue);
+};
 
 const newSearch = () => {
-   if (searcher.value) {
-      searcher.value = "";
-   }
-}
+  if (searcher.value) {
+    searcher.value = "";
+  }
+};
 
-const dataList = document.getElementById("optionsList");
-const dataListFragment = document.createDocumentFragment();
+const createOptionsList = () => {
+  const dataList = document.getElementById("optionsList");
+  const dataListFragment = document.createDocumentFragment();
 
-for(let i = 0; i < products.length; i++) {
-   const item = document.createElement("OPTION");
-   item.setAttribute("value", `${products[i]["Product Code"]}`);
-   dataListFragment.appendChild(item);
-}
+  for (let i = 0; i < products.length; i++) {
+    const item = document.createElement("OPTION");
+    item.setAttribute("value", `${products[i]["Product Code"]}`);
+    dataListFragment.appendChild(item);
+  }
 
-dataList.appendChild(dataListFragment);
+  dataList.appendChild(dataListFragment);
+};
 
-const table = document.getElementById("table");
-const tableFragment = document.createDocumentFragment();
+const createTableContent = () => {
+  const table = document.getElementById("table");
+  const tableFragment = document.createDocumentFragment();
 
-for(let i = 0; i < products.length; i++) {
-   const rowItem = document.createElement("TR");
-   let iD = "product" + i;
-   rowItem.setAttribute("id", iD);
+  for (let i = 0; i < products.length; i++) {
+    const rowItem = document.createElement("TR");
+    let iD = "product" + i;
+    rowItem.setAttribute("id", iD);
 
-   if(products[i]["Not Placed"] == 0) {
+    if (products[i]["Not Placed"] == 0) {
       rowItem.innerHTML = `
-         <td>${products[i]["Product Code"]}</td>
-         <td>${products[i]["Quantity"]}</td>
-         <td>${products[i]["Placed"]}</td>
-         <td style="color:#ffff00; background:#ff0000; font-weight:bolder; font-family:sans-serif">${products[i]["Not Placed"]}</td>
-         <td>${products[i]["Destination List"]}</td>`;
-   } else if(products[i]["Not Placed"] < 0) {
+            <td>${products[i]["Product Code"]}</td>
+            <td>${products[i]["Quantity"]}</td>
+            <td>${products[i]["Placed"]}</td>
+            <td style="color:#ffff00; background:#ff0000; font-weight:bolder; font-family:sans-serif">${products[i]["Not Placed"]}</td>
+            <td>${products[i]["Destination List"]}</td>`;
+    } else if (products[i]["Not Placed"] < 0) {
       rowItem.innerHTML = `
-         <td>${products[i]["Product Code"]}</td>
-         <td>${products[i]["Quantity"]}</td>
-         <td>${products[i]["Placed"]}</td>
-         <td style="color:#f3f3f3; background:#0000ff; font-weight:bolder; font-family:sans-serif">${products[i]["Not Placed"]}</td>
-         <td>${products[i]["Destination List"]}</td>`;
-   } else {
+            <td>${products[i]["Product Code"]}</td>
+            <td>${products[i]["Quantity"]}</td>
+            <td>${products[i]["Placed"]}</td>
+            <td style="color:#f3f3f3; background:#0000ff; font-weight:bolder; font-family:sans-serif">${products[i]["Not Placed"]}</td>
+            <td>${products[i]["Destination List"]}</td>`;
+    } else {
       rowItem.innerHTML = `
-         <td>${products[i]["Product Code"]}</td>
-         <td>${products[i]["Quantity"]}</td>
-         <td>${products[i]["Placed"]}</td>
-         <td>${products[i]["Not Placed"]}</td>
-         <td>${products[i]["Destination List"]}</td>`;
-   }
+            <td>${products[i]["Product Code"]}</td>
+            <td>${products[i]["Quantity"]}</td>
+            <td>${products[i]["Placed"]}</td>
+            <td>${products[i]["Not Placed"]}</td>
+            <td>${products[i]["Destination List"]}</td>`;
+    }
 
-   tableFragment.appendChild(rowItem);
-}
+    tableFragment.appendChild(rowItem);
+  }
 
-table.appendChild(tableFragment);
+  table.appendChild(tableFragment);
+};
 
-form1.addEventListener("input", function(){showProduct(searcher.value);});
+form1.addEventListener("input", function () {
+  showProduct(searcher.value);
+});
 searcher.addEventListener("click", newSearch);
-button1.addEventListener("click", function(){addProduct(searcher.value);});
-button2.addEventListener("click", function(){subtractProduct(searcher.value);});
-button3.addEventListener("click", function(){resetCount(searcher.value);});
-button4.addEventListener("click", function(){resetProduct(searcher.value);});
-button5.addEventListener("click", function(){window.print();});
+button1.addEventListener("click", function () {
+  addProduct(searcher.value);
+});
+button2.addEventListener("click", function () {
+  subtractProduct(searcher.value);
+});
+button3.addEventListener("click", function () {
+  resetCount(searcher.value);
+});
+button4.addEventListener("click", function () {
+  resetProduct(searcher.value);
+});
+button5.addEventListener("click", function () {
+  window.print();
+});
